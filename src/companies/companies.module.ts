e341433f5f4b97c83507564025d68f88d419e99b
kow111 <chome710@gmail.com>
2025-04-15 19:21:47 +0700

    create, update company

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,9 +3,11 @@ import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Company, CompanySchema } from './schemas/company.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }]),
   ],
   controllers: [CompaniesController],
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,21 +4,33 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { SoftDeleteModel } from 'mongoose-delete';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
+import { IUser } from 'src/users/users.interface';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CompaniesService {
   constructor(
     @InjectModel(Company.name)
     private companyModel: SoftDeleteModel<CompanyDocument>,
+    private readonly userService: UsersService,
   ) {}
 
-  async create(createCompanyDto: CreateCompanyDto) {
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
     try {
-      const company = await this.companyModel.create(createCompanyDto);
+      const { email } = user;
+      const foundUser = await this.userService.findOneByUsername(email);
+      if (!foundUser) {
+        throw new Error('User not found');
+      }
+      const company = await this.companyModel.create({
+        ...createCompanyDto,
+        createdBy: foundUser._id,
+        updatedBy: foundUser._id,
+      });
       return company;
     } catch (error) {
       console.error('Error creating company:', error);
-      throw new Error('Failed to create company');
+      throw new Error(`Failed to create company ${error.message}`);
     }
   }
 
@@ -30,8 +42,26 @@ export class CompaniesService {
     return `This action returns a #${id} company`;
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
+    try {
+      const { email } = user;
+      const foundUser = await this.userService.findOneByUsername(email);
+      if (!foundUser) {
+        throw new Error('User not found');
+      }
+      const company = await this.companyModel.findByIdAndUpdate(
+        id,
+        {
+          ...updateCompanyDto,
+          updatedBy: foundUser._id,
+        },
+        { new: true },
+      );
+      return company;
+    } catch (error) {
+      console.error('Error updating company:', error);
+      throw new Error(`Failed to update company ${error.message}`);
+    }
   }
 
   remove(id: number) {
